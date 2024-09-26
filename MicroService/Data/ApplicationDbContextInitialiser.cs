7c13a21aa1b2dd357e6b4e6b357dac8f3f8e7bfa
NotScream73 <112487179+NotScream73@users.noreply.github.com>
2024-09-26 20:33:04 +0400

    lab 1 (#2)

---
+++ MicroService/Data/ApplicationDbContextInitialiser.cs
@@ -0,0 +1,55 @@
+﻿using MicroService.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace MicroService.Data;
+
+public static class InitialiserExtensions
+{
+    public static async Task InitialiseDatabaseAsync(this WebApplication app)
+    {
+        using var scope = app.Services.CreateScope();
+
+        var initialiser = scope.ServiceProvider.GetRequiredService<ApplicationDbContextInitialiser>();
+
+        await initialiser.InitialiseAsync();
+
+        await initialiser.SeedAsync();
+    }
+}
+
+public class ApplicationDbContextInitialiser
+{
+    private readonly ApplicationDbContext _context;
+
+    public ApplicationDbContextInitialiser(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task InitialiseAsync()
+    {
+        await _context.Database.MigrateAsync();
+    }
+
+    public async Task SeedAsync()
+    {
+        await TrySeedAsync();
+    }
+
+    public async Task TrySeedAsync()
+    {
+        if (!_context.Students.Any())
+        {
+            _context.Students.Add(new Student
+            {
+                FirstName = "Никита",
+                LastName = "Сергеев",
+                MiddleName = "Игоревич",
+                Speciality = "Программный инженер",
+                IsExpelled = false
+            });
+
+            await _context.SaveChangesAsync();
+        }
+    }
+}
