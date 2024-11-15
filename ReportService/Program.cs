d6073cdd043fafe6b1b55d117318247db9985034
Никита Сергеев <notscreamplay73@gmail.com>
2024-11-15 22:04:48 +0400

    fix github

--- ReportService/Program.cs
+++ ReportService/Program.cs
@@ -45,12 +45,8 @@ builder.Services.AddHttpClient<ExternalClientService>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
+app.UseSwagger();
+app.UseSwaggerUI();
 //app.UseHttpsRedirection();
 app.UseMiddleware<TraceLoggingMiddleware>();
 
